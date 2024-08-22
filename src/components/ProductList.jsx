0891d28b96f73cfca33afcb278a253c977e96c7d
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-08-22 21:30:33 +0600

    get data from api and chack console log

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+
+const productsPerPage = 10;
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+
+  const loaderRef = useRef(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=${productsPerPage}&skip=${
+          page * productsPerPage
+        }`
+      );
+      const data = await response.json();
+      console.log("data is working", data);
+    };
+
+    const onIntersection = (items) => {
+      const loaderItem = items[0];
+
+      if (loaderItem.isIntersecting && hasMore) {
+        fetchProducts();
+      }
+    };
+
+    const observer = new IntersectionObserver(onIntersection);
+
+    if (observer && loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+
+    //clennup
+    return () => {
+      if (observer) observer.disconnect();
+    };
+  }, [hasMore, page]);
+
   return (
     <div>
       <div>ProductList</div>
 
       {/* product list witll be loaded */}
-      <div>Loading more products...</div>
+      <div ref={loaderRef}>Loading more products...</div>
     </div>
   );
 };
